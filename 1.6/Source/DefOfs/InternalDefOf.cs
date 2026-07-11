090b534b56e5e2f4ceefe49b39ee08a5e3195b8a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-07-11 15:54:02 +0200

    Initial commit

---
+++ 1.6/Source/DefOfs/InternalDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+namespace VanillaRanchingExpanded
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+		static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+		
+	}
+}
---
+++ 1.6/Source/Harmony/HarmonyInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+namespace VanillaRanchingExpanded
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.VanillaRanchingExpanded");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
