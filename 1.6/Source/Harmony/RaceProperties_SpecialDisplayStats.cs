e6ee7af758b91270043fd51e3c1fd63f925a8b21
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-08-13 11:34:52 +0200

    v0.1

---
+++ 1.6/Source/Harmony/RaceProperties_SpecialDisplayStats.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Verse;
+using System;
+using System.Reflection;
+
+namespace VanillaRanchingExpanded
+{
+    [HarmonyPatch]
+    public static class VanillaRanchingExpanded_RaceProperties_SpecialDisplayStats_Patch
+    {
+        public static MethodInfo moveNext;
+
+        public static MethodBase TargetMethod()
+        {
+            moveNext = AccessTools.EnumeratorMoveNext(AccessTools.Method(typeof(RaceProperties), "SpecialDisplayStats"));
+            return moveNext;
+        }
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> ModifyLifespan(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+            var lifeExpectancyField = AccessTools.Field(typeof(RaceProperties), "lifeExpectancy");
+            var reqField = AccessTools.Field(moveNext.DeclaringType, "req");
+ 
+            var adjustLifeExpectancyMethod = AccessTools.Method(typeof(VanillaRanchingExpanded_RaceProperties_SpecialDisplayStats_Patch), "AdjustLifeExpectancy");
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+
+                if (codes[i].opcode == OpCodes.Ldfld && codes[i].OperandIs(lifeExpectancyField))
+                {
+                    yield return codes[i];
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldfld, reqField);
+                    yield return new CodeInstruction(OpCodes.Call, adjustLifeExpectancyMethod);
+                }
+               
+                else yield return codes[i];
+            }
+        }
+
+        public static float AdjustLifeExpectancy(float expectancy, StatRequest req)
+        {
+            Pawn pawn = req.Thing as Pawn;
+            if (pawn != null && WorldComponent_AnimalGenes.Instance.pawnToCompAnimalGenes.ContainsKey(pawn)) {
+                CompAnimalGenes comp = WorldComponent_AnimalGenes.Instance.pawnToCompAnimalGenes[pawn];
+                if (comp != null) {
+                    return expectancy * comp.LifeSpanFactor;
+                }
+            }
+
+            return expectancy;
+        }
+
+      
+
+    }
+}
